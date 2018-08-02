bf09ff39e8de4f29ca1fe75dfa28c7689320e1b0
Oyvind Sorlie <heiastart@outlook.com>
2018-08-02 14:00:57 +0200

    Test-commit...

--- App4_website/script1.py
+++ App4_website/script1.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-@app.route('/')
 
+@app.route('/')
 def home():
-    return "Website content goes here!!!"
+    return render_template('home.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/layout/')
+def layout():
+    return render_template('layout.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
