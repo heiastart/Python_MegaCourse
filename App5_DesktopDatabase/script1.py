5a4c607e5afbbabc2e68b88ef4f29673e34799ea
Oyvind Sorlie <heiastart@outlook.com>
2018-08-12 15:11:53 +0200

    Startet App5

---
+++ App5_DesktopDatabase/script1.py
@@ -0,0 +1,13 @@
+'''
+A program that stores the following book information;
+Title, author
+Year, ISBN
+
+The user can (with buttons/widgets);
+- View all records
+- Search an entry
+- Add entry
+- Update entry
+- Delete entry
+- Close the program/window
+'''
